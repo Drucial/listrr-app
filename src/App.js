2fa7ac55fbcc3d0c04e8a1b71190ad8685997de6
Drucial <jdwhite32@gmail.com>
2021-11-22 17:00:21 -0500

    mobile viewport height

--- src/App.js
+++ src/App.js
@@ -68,4 +68,11 @@ function App() {
   );
 }
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 export default App;
